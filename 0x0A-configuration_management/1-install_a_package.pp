e40fd2a05238c136e9015de81469f6e4afeea6f9
EDEN <issamghled@gmail.com>
2024-04-04 05:19:17 +0000

    4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/env puppet
-
-# Install a specific version of Flask (2.1.0)
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
